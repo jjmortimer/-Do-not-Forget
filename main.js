aeb63b4d654f4a9c1bd653486cd79761d490825c
jjmortimer <73339030+jjmortimer@users.noreply.github.com>
2020-11-28 22:42:10 +0000

    Lumen added cSS

--- main.js
+++ main.js
@@ -1,20 +1,46 @@
 var todos = [];
 
 function addToDo() {
-  var inputValue = $('#todo1').val();
+  var inputValue = $('#input').val();
+  if (!inputValue) return;
+  $('#input').val('');
   $("#todos").append('<div class="todo">' + inputValue + '</div>');
   todos.push(inputValue);
-  console.log(todos);
   localStorage.setItem('todos', JSON.stringify(todos));
+
+  completeTasks();
+}
+
+function completeTasks() {
+  $('.todo').click(function(e) {
+  	// Remove element
+    $(e.target).remove();
+    var value = $(e.target).text();
+    // Remove from array
+    var index = todos.indexOf(value);
+    if (index !== -1) { todos.splice(index, 1); }
+    // Save updated todos
+    localStorage.setItem('todos', JSON.stringify(todos));
+  });
 }
 
 $(document).ready(function() {
-    $("#my-button").bind("click", addToDo);
-  var todos = localStorage.getItem('todos');
-    todos = JSON.parse(todos);
+  // Bind functions
+  $("#my-button").bind("click", addToDo);
+  $("#input").bind('keypress', function(e) {
+    if (e.which == 13) {
+      addToDo();
+    }
+  });
 
+  // Load saved todos
+  todos = localStorage.getItem('todos');
+  if (!todos) return;
+  todos = JSON.parse(todos);
   for (i = 0; i < todos.length; i++) {
     $("#todos").append('<div class="todo">' + todos[i] + '</div>');
   }
 
+  // Bind complete tasks function to todos
+  completeTasks();
 });
